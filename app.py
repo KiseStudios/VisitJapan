aac6c9cbe49ad4f9a758400fe0f75b8c6ba2d4ff
Kise Studios <kisestudios@hotmail.com>
2025-11-16 17:27:02 -0300

    app.py

--- app.py
+++ app.py
@@ -18,10 +18,13 @@ def galeria():
 def contatos():
     return render_template('contatos.html')  # Página contatos
 
+@app.route('/cultura')
+def cultura():
+    return render_template('cultura.html')  # Página cultura
+
 @app.route('/tecnologia')
 def tecnologia():
     return render_template('tecnologia.html')  # Página tecnologia
-
 
 if __name__ == '__main__':
     app.run(debug=True)
